b7b98a03320bdc3b8dfeb73ebe74d3a09c53fc75
gwni0214 <sbxjs6999@naver.com>
2023-05-12 23:35:43 +0900

    asd

---
+++ linktree/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>링크트리 만들기</h1>
+    <div>
+      <a href="https://www.naver.com/" target="_blank">네이버로 이동하기</a>
+    </div>
+  
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
