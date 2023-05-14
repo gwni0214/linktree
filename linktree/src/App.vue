192cb4ad0524e61c9e856be2524b454fd61a1680
gwni0214 <sbxjs6999@naver.com>
2023-05-14 09:23:55 +0900

    박근희

--- linktree/src/App.vue
+++ linktree/src/App.vue
@@ -1,22 +1,24 @@
 <template>
-  <div>
-    <h1>링크트리 만들기</h1>
-    <div>
-      <a href="https://www.naver.com/" target="_blank">네이버로 이동하기</a>
+  <div class="body">
+    <h1 class="title">링크트리 만들기</h1>
+    <div class="content">
+      <div class="box">
+        <a class="naver" href="https://www.naver.com/" target="_blank">네이버로 이동하기</a>
+      </div>
+      <a class="naver" id="ccc" href="https://marpple.shop/kr/chillcocktailclub?page=0" target="_blank">칠칵테일 클럽으로 이동하기</a>
     </div>
-  
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
@@ -26,8 +28,49 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
+}
+.body {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+}
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.title {
+  color: maroon;
+  user-select: none;
+}
+a {
+  color: black;
+}
+.naver {
+  color: pink;
+}
+#ccc {
+  color: blue;
+}
+.board_category {
+
+}
+.category {
+
+}
+.box {
+  width: 400px;
+  height: 200px;
+  background-color: #fff;
+  text-align: center;
+  margin-top: 50px;
+}
+.naver.name {
+  margin-top : 50px;
 }
 </style>
